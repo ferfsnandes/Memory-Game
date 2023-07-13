303a270469010bdafc9e0f7fdfa5ad5284b377bd
ferfsnandes <112121591+ferfsnandes@users.noreply.github.com>
2023-07-13 15:01:49 -0300

    Add files via upload

---
+++ modify.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+$name = $_POST['nome'];
+$email = $_POST['email'];
+$phone = $_POST['phnumber'];
+$password = $_POST['psw'];
+
+
+$servername = 'localhost';
+$username = 'root';
+$dbname = 'jogo_da_memoria';
+$connect = new mysqli("localhost","root",'',"jogo_da_memoria");
+
+
+if ($connect-> connect_error) {
+       die('connection failed :' .$connect-> $connect_error);
+}
+
+else {
+       $sql = ("UPDATE usuario
+       SET nome = '$name', 
+       telefone = '$phone', 
+       email = '$email', 
+       senha = '$password'
+       WHERE login = '" . $_SESSION["sess_user"] ."';");
+}
+
+$sql = mysqli_query($connect, $sql);
+
+
+if ($sql == true) {
+       header("Location: index.html"); 
+} else {
+       echo "Usuário não foi salvo.";
+}
+
+?> 
