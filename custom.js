bfb53955f6e8d2ec843b6e1dc12418b57674bb08
ferfsnandes <112121591+ferfsnandes@users.noreply.github.com>
2024-10-05 15:13:00 -0300

    atualizando visual

---
+++ custom.js
@@ -0,0 +1,6 @@
+// You can add this in custom.js
+window.onclick = function(event) {
+    if (event.target == document.getElementById('id01')) {
+        document.getElementById('id01').style.display = 'none';
+    }
+}
