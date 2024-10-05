eb2ef8a7eab1903c46f76bfe47dfb881e04eed92
ferfsnandes <112121591+ferfsnandes@users.noreply.github.com>
2024-10-05 14:22:35 -0300

    Revert "Merge branch 'main' of https://github.com/ferfsnandes/Memory-Game"
    This reverts commit c716bd884332c9bd6297edb34fe58f3e1ddaed25, reversing
    changes made to d16972d5f0ffd0e96c62907e4dcf1276629a7ed8.

--- logging.php
+++ logging.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
    <?php  
 if(isset($_POST["submit"])){  
   
@@ -36,42 +35,3 @@ if(!empty($_POST['uname']) && !empty($_POST['pass'])) {
 }  
 }  
 ?> 
-=======
-   <?php  
-if(isset($_POST["submit"])){  
-  
-if(!empty($_POST['uname']) && !empty($_POST['pass'])) {  
-    $user=$_POST['uname'];  
-    $pass=$_POST['pass'];  
-  
-    $con=mysqli_connect('localhost','root','') or die(mysqli_error());  
-    mysqli_select_db($con,'jogo_da_memoria') or die("Banco de dados não pôde ser conectado");  
-  
-    $query=mysqli_query($con,"SELECT * FROM usuario WHERE login='".$user."' AND senha='".$pass."'");  
-    $numrows=mysqli_num_rows($query);  
-    if($numrows!=0)
-    {  
-    while($row=mysqli_fetch_assoc($query))  
-    {  
-    $dbusername=$row['login'];  
-    $dbpassword=$row['senha'];  
-    }  
-  
-    if($user == $dbusername && $pass == $dbpassword)  
-    {  
-    session_start();  
-    $_SESSION['sess_user']=$user;  
-  
-    /* Redirect browser to another page */  
-    header("Location: escolhaGAME.html");  
-    }  
-    } else {  
-    echo "Usuário ou senha inválido.";  
-    }  
-  
-} else {  
-    echo "Todos os campos devem ser preenchidos.";  
-}  
-}  
-?> 
->>>>>>> 303a270469010bdafc9e0f7fdfa5ad5284b377bd
